414ee167901b542cf48ada394267428a3b166cc8
zhangjiayu <zhangjiayu@meituan.com>
2016-02-25 09:34:50 +0800

    修改

--- src/main/java/com/sankuai/chatserver/bootstrap/NettyBootStrap.java
+++ src/main/java/com/sankuai/chatserver/bootstrap/NettyBootStrap.java
@@ -1,8 +1,18 @@
 package com.sankuai.chatserver.bootstrap;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+import com.sankuai.chatserver.handler.WebSocketHandler;
 
 public class NettyBootStrap {
 
@@ -13,9 +23,24 @@ public class NettyBootStrap {
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
 		try {
 			ServerBootstrap sb = new ServerBootstrap();
-			
+			sb.group(bossGroup, workerGroup).channel(ServerSocketChannel.class)
+					.childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						public void initChannel(SocketChannel ch)
+								throws Exception {
+							ChannelPipeline pipeline = ch.pipeline();
+							pipeline.addLast(new HttpServerCodec());
+							// 聚合http片段
+							pipeline.addLast(new HttpObjectAggregator(65536));
+							// 支持HTML5 文本传输
+							pipeline.addLast(new ChunkedWriteHandler());
+							pipeline.addLast(new WebSocketHandler());
+						}
+					});
+			ChannelFuture f = sb.bind(port).sync();
+			f.channel().closeFuture().sync();
 		} catch (Exception e) {
-
+			// TODO
 		} finally {
 			workerGroup.shutdownGracefully();
 			bossGroup.shutdownGracefully();
@@ -23,6 +48,6 @@ public class NettyBootStrap {
 	}
 
 	public static void main(String[] args) {
-
+		run();
 	}
 }
---
+++ src/main/java/com/sankuai/chatserver/handler/WebSocketHandler.java
@@ -0,0 +1,42 @@
+package com.sankuai.chatserver.handler;
+
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+
+public class WebSocketHandler extends ChannelHandlerAdapter {
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		// TODO Auto-generated method stub
+		super.channelActive(ctx);
+	}
+
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+		// TODO Auto-generated method stub
+		super.channelInactive(ctx);
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg)
+			throws Exception {
+		super.channelRead(ctx, msg);
+		if (msg instanceof FullHttpRequest) {
+			handleWebsocketConnection();
+		} else if (msg instanceof WebSocketFrame) {
+			handleWebsocketRequest();
+		} else {
+			ctx.fireChannelRead(msg);
+		}
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+			throws Exception {
+		// TODO Auto-generated method stub
+		super.exceptionCaught(ctx, cause);
+	}
+
+}
